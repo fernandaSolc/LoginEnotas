59226e64c81a189d7571507e2589f161a85bfef3
fernandaSolc <130673034+fernandaSolc@users.noreply.github.com>
2023-12-01 14:19:48 +0000

    Readme

--- .ccls-cache/@home@runner@LoginEnotas/src@main.c
+++ .ccls-cache/@home@runner@LoginEnotas/src@main.c
@@ -2,53 +2,46 @@
 #include "login.h"
 #include "notas.h"
 
+char login[6],
+senha[10];
+char opcao;
+int userExists;
+
 main() {
-    char login[6],
-    senha[10];
+
 
     carregarDados();
 
     printf("Digite seu login: ");
     scanf("%s", login);
 
-    int index = encontrarUsuario(login);
-    if (index == -1) 
-      {
+     int index = encontrarUsuario(login);
+  
         printf("Digite sua senha: ");
         scanf("%s", senha);
         if (verificarSenha(index, senha)) 
         {
             printf("Login bem sucedido!\n");
-      }
+        }
     
      else {
       printf("Usuario nao encontrado. Deseja criar uma conta? (s/n): ");
-      char opcao;
       scanf(" %c", &opcao);
-      
-       if (opcao == 's') {
-           int userExists;
-           do {
-               printf("Digite seu novo login: ");
-               scanf("%s", login);
-             
-               userExists = encontrarUsuario(login);
-             
-               if (userExists != -1) {
-                   printf("Login ja existente. Por favor, escolha outro login.\n");
-               }
-           } while (userExists != -1);
-           printf("Digite sua senha: ");
-           scanf("%s", senha);
-           cadastrarUsuario(login, senha);
-           
-    } 
-      else 
-      {
+     
+    }
+}         
+
+void salvarUsuario() {
+if (opcao == 's') {
+    printf("Digite seu novo login: ");
+    scanf("%s", login);
+    int userExists = encontrarUsuario(login);
+    if (userExists != -1) {
+        printf("Login ja existente. Por favor, escolha outro login.\n\n");
+    } else {
         printf("Digite sua senha: ");
         scanf("%s", senha);
         cadastrarUsuario(login, senha);
-        printf("\n\nConta criada com sucesso!");
-    }
-      }
-  }}
+        salvarUsuario();
+        printf("\n\nConta criada com sucesso!"); }
+}}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Criador de login e calculador de notas (in progress)
+
+### Código apresenta alguns erros, ainda em progresso.
